8d71488d8dcc1ce12b6292dd5076a0cbfa512e46
christancojocaru <christancojocaru@gmail.com>
2019-10-14 16:01:29 +0300

    need rethink

--- src/index.js
+++ src/index.js
@@ -98,170 +98,190 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            name: this.props.name,
             room_id: this.props.room_id,
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            stepNumber: 0,
-            xIsNext: true,
-            hovered: Array(9).fill(false),
-            order: true,
+            IamNext: true,
             won: false,
-            clickMe: false,
-            playerSelected: false,
-            name: this.props.name
+            isPlayerSelected: false,
+            player: null,
+            // history: [{
+                // squares: Array(9).fill(null),
+            // }],
+            // stepNumber: 0,
+            // hovered: Array(9).fill(false),
+            // order: true,
+            // clickMe: false,
         };
     }
 
-    handleClick(i) {
-        const xIsNext = this.state.xIsNext ? 'X' : 'O';
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        const winner = calculateWinner(squares);
-        const squares_modified = squares.slice();
-        squares_modified.splice(i, 1, xIsNext);
-        console.log(calculateWinner(squares_modified));
-
-        if (calculateWinner(squares_modified)) {
-            let winner = calculateWinner(squares_modified);
-            let hovered = Array(9).fill(false);
-            hovered.forEach(
-                (h_V, h_I) => {
-                    winner[1].forEach(
-                        (l_V) => {
-                            if (h_I === l_V) {
-                                hovered.splice(h_I, 0, true);
-                            }
-                        });
-                });
+    componentDidMount() {
+        socket.on("selectedPlayer", (data) => {
             this.setState({
-                hovered: hovered,
+                player: data.player === "X" ? "O" : "X",
             });
-        }
-        if (squares[i] || winner[0]) {
-            this.setState({
-                won: true
-            });
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares,
-                grid: getColAndRow(i),
-            }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
+        });
+
+        socket.on("friend_disconnected", () => {
+            changeConnectionToGame(this.state.name);
         });
     }
-
-    jumpTo(index, step) {
-        console.log("index = " + index + "\n step = " + step.squares );
-        let current_xIxNext = this.state.xIsNext;
-        let current_stepNumber = this.state.stepNumber;
-        let new_xIsNext;
-        if (index === 0) {
-            new_xIsNext = (current_stepNumber % 2) === 0 ? current_xIxNext : !current_xIxNext;
-        } else {
-            new_xIsNext = ( (current_stepNumber - index) % 2) === 0 ? current_xIxNext : !current_xIxNext;
-        }
-        this.setState({
-            stepNumber: index,
-            xIsNext: new_xIsNext,
-        });
-    }
-
-    highlightsSquare(step) {
-        const history_squares = this.state.history[this.state.history.length - 1].squares.slice();
-        const step_squares = step.squares;
-
-        let hovered_result = [];
-        history_squares.forEach(
-            (h_s_V, h_s_I) => {
-                hovered_result[h_s_I] =
-                    (step_squares[h_s_I] === h_s_V) ?
-                        !!h_s_V :
-                        false;
-            });
-        this.setState({
-            hovered: hovered_result
-        });
-    }
-
-    unHighlightsSquare() {
-        this.setState({
-            hovered: Array(9).fill(false),
-        });
-    }
-
-    sort() {
-        this.setState({
-            order: !this.state.order
-        });
-    }
-
-    makeSortButton() {
-        return (
-            <li key="sort">
-                <button
-                    onClick={() => this.sort()}
-                    className="sort"
-                    title="sort moves"
-                >
-                    Sort the moves <span className="order">{this.state.order ? "desc" : "asc"}</span>
-                </button>
-            </li>
-        );
-    }
-
-    move(index, step = null, desc) {
-        return (
-            <li key={index}>
-                <button
-                    onClick={() => {
-                        this.jumpTo(index, step);
-                        if (index === 0) {
-                            this.setState({
-                                clickMe: false,
-                                hovered: Array(9).fill(false),
-                                history: [{
-                                    squares: Array(9).fill(null),
-                                }],
-                            });
-                        }
-                    }}
-                    onMouseOver={() => index === 0 ? "" : this.highlightsSquare(step)}
-                    onMouseLeave={() => index === 0 ? "" : this.unHighlightsSquare()}
-                >
-                    {desc}
-                </button>
-            </li>
-
-        );
-    }
-
-    clickMeButton() {
-        return (
-            <div className="click-me">
-                &#8601; Click me!
-            </div>
-        );
-    }
-
-    createPlayer(name) {
+    //
+    // handleClick(i) {
+    //     const xIsNext = this.state.xIsNext ? 'X' : 'O';
+    //     const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    //     const current = history[history.length - 1];
+    //     const squares = current.squares.slice();
+    //     const winner = calculateWinner(squares);
+    //     const squares_modified = squares.slice();
+    //     squares_modified.splice(i, 1, xIsNext);
+    //     console.log(calculateWinner(squares_modified));
+    //
+    //     if (calculateWinner(squares_modified)) {
+    //         let winner = calculateWinner(squares_modified);
+    //         let hovered = Array(9).fill(false);
+    //         hovered.forEach(
+    //             (h_V, h_I) => {
+    //                 winner[1].forEach(
+    //                     (l_V) => {
+    //                         if (h_I === l_V) {
+    //                             hovered.splice(h_I, 0, true);
+    //                         }
+    //                     });
+    //             });
+    //         this.setState({
+    //             hovered: hovered,
+    //         });
+    //     }
+    //     if (squares[i] || winner[0]) {
+    //         this.setState({
+    //             won: true
+    //         });
+    //         return;
+    //     }
+    //     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //     this.setState({
+    //         history: history.concat([{
+    //             squares: squares,
+    //             grid: getColAndRow(i),
+    //         }]),
+    //         stepNumber: history.length,
+    //         xIsNext: !this.state.xIsNext
+    //     });
+    // }
+    //
+    // jumpTo(index, step) {
+    //     console.log("index = " + index + "\n step = " + step.squares );
+    //     let current_xIxNext = this.state.xIsNext;
+    //     let current_stepNumber = this.state.stepNumber;
+    //     let new_xIsNext;
+    //     if (index === 0) {
+    //         new_xIsNext = (current_stepNumber % 2) === 0 ? current_xIxNext : !current_xIxNext;
+    //     } else {
+    //         new_xIsNext = ( (current_stepNumber - index) % 2) === 0 ? current_xIxNext : !current_xIxNext;
+    //     }
+    //     this.setState({
+    //         stepNumber: index,
+    //         xIsNext: new_xIsNext,
+    //     });
+    // }
+    //
+    // highlightsSquare(step) {
+    //     const history_squares = this.state.history[this.state.history.length - 1].squares.slice();
+    //     const step_squares = step.squares;
+    //
+    //     let hovered_result = [];
+    //     history_squares.forEach(
+    //         (h_s_V, h_s_I) => {
+    //             hovered_result[h_s_I] =
+    //                 (step_squares[h_s_I] === h_s_V) ?
+    //                     !!h_s_V :
+    //                     false;
+    //         });
+    //     this.setState({
+    //         hovered: hovered_result
+    //     });
+    // }
+    //
+    // unHighlightsSquare() {
+    //     this.setState({
+    //         hovered: Array(9).fill(false),
+    //     });
+    // }
+    //
+    // sort() {
+    //     this.setState({
+    //         order: !this.state.order
+    //     });
+    // }
+    //
+    // makeSortButton() {
+    //     return (
+    //         <li key="sort">
+    //             <button
+    //                 onClick={() => this.sort()}
+    //                 className="sort"
+    //                 title="sort moves"
+    //             >
+    //                 Sort the moves <span className="order">{this.state.order ? "desc" : "asc"}</span>
+    //             </button>
+    //         </li>
+    //     );
+    // }
+    //
+    // move(index, step = null, desc) {
+    //     return (
+    //         <li key={index}>
+    //             <button
+    //                 onClick={() => {
+    //                     this.jumpTo(index, step);
+    //                     if (index === 0) {
+    //                         this.setState({
+    //                             clickMe: false,
+    //                             hovered: Array(9).fill(false),
+    //                             history: [{
+    //                                 squares: Array(9).fill(null),
+    //                             }],
+    //                         });
+    //                     }
+    //                 }}
+    //                 onMouseOver={() => index === 0 ? "" : this.highlightsSquare(step)}
+    //                 onMouseLeave={() => index === 0 ? "" : this.unHighlightsSquare()}
+    //             >
+    //                 {desc}
+    //             </button>
+    //         </li>
+    //
+    //     );
+    // }
+    //
+    // clickMeButton() {
+    //     return (
+    //         <div className="click-me">
+    //             &#8601; Click me!
+    //         </div>
+    //     );
+    // }
+    //
+    createPlayer(player_name) {
         return (
             <option
-                onClick={() => this.setState({
-                    xIsNext: name === "X"
-                })}
+                selected={this.state.player === player_name}
+                onClick={() => {
+                  this.setState({
+                    player: player_name,
+                  });
+                  socket.emit("selectedPlayer", {
+                      room_id: this.state.room_id,
+                      player: player_name
+                  })
+                }}
             >
-                Player {name}</option>
+                Player {player_name}</option>
         );
     }
 
     render() {
-        if (!this.state.playerSelected) {
+        if (!this.state.isPlayerSelected) {
             return (
                 <div className="select-player">
                   <h3>Please Select a player Mr. {this.state.name} .</h3>
@@ -390,13 +410,10 @@ class Connection extends React.Component {
 
   componentDidMount() {
     let interval_id = setInterval(() => {
-      console.log("receiving Players");
       socket.on('receive_players', (players) => {
-        console.log(players);
         this.setState({players: players});
       });
 
-      console.log("receiving connection");
       socket.on('connect_player', (name) => {
         this.setState({player_want_you: name});
       });
@@ -415,6 +432,7 @@ class Connection extends React.Component {
         <div>
           <input id="name" type="text" placeholder="Please insert a name" />
           <button
+            id="send_name"
             onClick={() => {
               let value = document.getElementById("name").value;
               let data = {id: socket.id, name: value}
@@ -438,7 +456,11 @@ class Connection extends React.Component {
       return player.socket_id !== socket.id;
     });
     let data = players.map((player, id) => {
-      return (<Player key={id} name={player.name} player_want_you={player_want_you}/>);
+      return (<Player
+          key={id}
+          name={player.name}
+          player_want_you={player_want_you}
+          />);
     });
     return (
       <div>
